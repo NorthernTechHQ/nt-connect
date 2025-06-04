NorthernTechHQ/nt-connect

commit fab8d68fcb38f74c06f859fcd81371f255bcbb12

chore: Migrate websockets package to nhooyr.io/websocket
Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

api/ws/websocket.go
@@ -24,16 +24,16 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/mendersoftware/go-lib-micro/ws"
 	"github.com/vmihailenco/msgpack/v5"
+	"nhooyr.io/websocket"
 
 	"github.com/northerntechhq/nt-connect/api"
 )
 
 type socket struct {
 	msgChan  chan ws.ProtoMsg
-	errChan  chan error
+	err      error
 	pongChan chan struct{}
 	done     chan struct{}
 	mu       sync.Mutex
@@ -43,9 +43,6 @@ type socket struct {
 
 func (sock *socket) ReceiveChan() <-chan ws.ProtoMsg {
 	return sock.msgChan
-}
-func (sock *socket) ErrorChan() <-chan error {
-	return sock.errChan
 }
 
 var (
@@ -69,48 +66,29 @@ func (sock *socket) Send(msg ws.ProtoMsg) error {
 		}
 		sock.writeMu.Lock()
 		defer sock.writeMu.Unlock()
-		err = sock.conn.WriteMessage(websocket.BinaryMessage, b)
+		err = sock.conn.Write(sock, websocket.MessageBinary, b)
 	}
 	return err
 }
 
-func (sock *socket) term() bool {
+func (sock *socket) term(err error) bool {
 	sock.mu.Lock()
 	defer sock.mu.Unlock()
 	select {
 	case <-sock.done:
 		return true
 	default:
+		sock.err = err
 		close(sock.done)
 	}
 	return false
 }
 
 func (sock *socket) Close() error {
-	if !sock.term() {
-		data := websocket.FormatCloseMessage(
-			websocket.CloseNormalClosure,
-			"closing connection",
-		)
-		errCtrl := sock.conn.WriteControl(
-			websocket.CloseMessage,
-			data,
-			time.Time{},
-		)
-		err := sock.conn.Close()
-		if err == nil {
-			err = errCtrl
-		}
-		return err
+	if !sock.term(nil) {
+		return sock.conn.Close(websocket.StatusNormalClosure, "disconnecting")
 	}
 	return nil
-}
-
-func (sock *socket) pushError(err error) {
-	select {
-	case sock.errChan <- err:
-	default:
-	}
 }
 
 func (sock *socket) receiver() {
@@ -118,15 +96,16 @@ func (sock *socket) receiver() {
 	defer close(sock.msgChan)
 	for {
 		var msg ws.ProtoMsg
-		_, r, err := sock.conn.NextReader()
+		_, r, err := sock.conn.Reader(sock)
 		if err != nil {
-			sock.pushError(err)
+			sock.term(err)
 			return
 		}
 		err = msgpack.NewDecoder(r).
 			Decode(&msg)
 		if err != nil {
-			sock.pushError(err)
+			sock.term(err)
+			return
 		}
 		select {
 		case <-sock.done:
@@ -134,16 +113,6 @@ func (sock *socket) receiver() {
 		case sock.msgChan <- msg:
 		}
 	}
-}
-
-func (sock *socket) ping() error {
-	sock.writeMu.Lock()
-	defer sock.writeMu.Unlock()
-	return sock.conn.WriteControl(
-		websocket.PingMessage,
-		nil,
-		time.Time{},
-	)
 }
 
 func (sock *socket) pinger() {
@@ -161,9 +130,9 @@ func (sock *socket) pinger() {
 		case <-sock.done:
 			return
 		case <-ticker.C:
-			err := sock.ping()
+			err := sock.conn.Ping(sock)
 			if err != nil {
-				sock.pushError(err)
+				sock.term(err)
 				return
 			}
 			// Generous deadline of 30 secs
@@ -176,7 +145,7 @@ func (sock *socket) pinger() {
 				}
 			}
 		case <-timer.C:
-			sock.pushError(ErrPongDeadline)
+			sock.term(ErrPongDeadline)
 			return
 		}
 	}
@@ -185,23 +154,11 @@ func (sock *socket) pinger() {
 func newSocket(conn *websocket.Conn) (*socket, error) {
 	sock := &socket{
 		msgChan:  make(chan ws.ProtoMsg),
-		errChan:  make(chan error, 1),
 		pongChan: make(chan struct{}, 1),
 		done:     make(chan struct{}),
 		conn:     conn,
 	}
-	conn.SetPongHandler(func(appData string) error {
-		select {
-		case sock.pongChan <- struct{}{}:
-		default:
-		}
-		return nil
-	})
-	conn.SetCloseHandler(func(code int, text string) error {
-		sock.term()
-		return nil
-	})
-	err := sock.ping()
+	err := sock.conn.Ping(sock)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to server: %w", err)
 	}
@@ -210,15 +167,39 @@ func newSocket(conn *websocket.Conn) (*socket, error) {
 	return sock, nil
 }
 
-// Client implements only parts of the api.Client interface
-type Client websocket.Dialer
+// Done extends the socket to use itself as a Context
+func (sock *socket) Done() <-chan struct{} {
+	return sock.done
+}
 
-func NewClient(tlsConfig *tls.Config) *Client {
-	return (*Client)(&websocket.Dialer{
-		Proxy:            http.ProxyFromEnvironment,
-		TLSClientConfig:  tlsConfig.Clone(),
-		HandshakeTimeout: time.Minute,
-	})
+// Err extends the socket to use itself as a Context
+func (sock *socket) Err() error {
+	if sock.err != nil {
+		return sock.err
+	}
+	select {
+	case <-sock.done:
+		return context.Canceled
+	default:
+		return nil
+	}
+}
+
+// Deadline extends the socket to use itself as a Context
+func (sock *socket) Deadline() (time.Time, bool) {
+	return time.Time{}, false
+}
+
+// Value extends the socket to use itself as a Context
+func (sock *socket) Value(key any) any {
+	return nil
+}
+
+// Client implements only parts of the api.Client interface
+type Client struct{}
+
+func NewClient(tlsConfig *tls.Config) api.SocketClient {
+	return &Client{}
 }
 
 func (c *Client) OpenSocket(ctx context.Context, authz *api.Authz) (api.Socket, error) {
@@ -227,16 +208,15 @@ func (c *Client) OpenSocket(ctx context.Context, authz *api.Authz) (api.Socket, 
 	if strings.HasPrefix(url, "http") {
 		url = strings.Replace(url, "http", "ws", 1)
 	}
-	conn, rsp, err := (*websocket.Dialer)(c).DialContext(
-		ctx, url, http.Header{
+	//nolint: bodyclose
+	conn, rsp, err := websocket.Dial(ctx,
+		url,
+		&websocket.DialOptions{HTTPHeader: http.Header{
 			"Authorization": []string{"Bearer " + authz.Token},
-		},
+		}},
 	)
 	if err != nil {
 		return nil, err
-	}
-	if rsp.Body != nil {
-		_ = rsp.Body.Close()
 	}
 	if rsp.StatusCode >= 300 {
 		return nil, &api.Error{Code: rsp.StatusCode}

api/dbus/client.go
@@ -40,7 +40,7 @@ type ClientDBus struct {
 	dbusAPI          dbus.DBusAPI
 	dbusConnection   dbus.Handle
 	authManagerProxy dbus.Handle
-	wsClient         *ws.Client
+	wsClient         api.SocketClient
 }
 
 var _ api.Client = &ClientDBus{}

vendor/github.com/gorilla/websocket/client.go
@@ -1,444 +1,0 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package websocket
-
-import (
-	"bytes"
-	"context"
-	"crypto/tls"
-	"errors"
-	"fmt"
-	"io"
-	"log"
-
-	"net"
-	"net/http"
-	"net/http/httptrace"
-	"net/url"
-	"strings"
-	"time"
-
-	"golang.org/x/net/proxy"
-)
-
-// ErrBadHandshake is returned when the server response to opening handshake is
-// invalid.
-var ErrBadHandshake = errors.New("websocket: bad handshake")
-
-var errInvalidCompression = errors.New("websocket: invalid compression negotiation")
-
-// NewClient creates a new client connection using the given net connection.
-// The URL u specifies the host and request URI. Use requestHeader to specify
-// the origin (Origin), subprotocols (Sec-WebSocket-Protocol) and cookies
-// (Cookie). Use the response.Header to get the selected subprotocol
-// (Sec-WebSocket-Protocol) and cookies (Set-Cookie).
-//
-// If the WebSocket handshake fails, ErrBadHandshake is returned along with a
-// non-nil *http.Response so that callers can handle redirects, authentication,
-// etc.
-//
-// Deprecated: Use Dialer instead.
-func NewClient(netConn net.Conn, u *url.URL, requestHeader http.Header, readBufSize, writeBufSize int) (c *Conn, response *http.Response, err error) {
-	d := Dialer{
-		ReadBufferSize:  readBufSize,
-		WriteBufferSize: writeBufSize,
-		NetDial: func(net, addr string) (net.Conn, error) {
-			return netConn, nil
-		},
-	}
-	return d.Dial(u.String(), requestHeader)
-}
-
-// A Dialer contains options for connecting to WebSocket server.
-//
-// It is safe to call Dialer's methods concurrently.
-type Dialer struct {
-	// NetDial specifies the dial function for creating TCP connections. If
-	// NetDial is nil, net.Dial is used.
-	NetDial func(network, addr string) (net.Conn, error)
-
-	// NetDialContext specifies the dial function for creating TCP connections. If
-	// NetDialContext is nil, NetDial is used.
-	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
-
-	// NetDialTLSContext specifies the dial function for creating TLS/TCP connections. If
-	// NetDialTLSContext is nil, NetDialContext is used.
-	// If NetDialTLSContext is set, Dial assumes the TLS handshake is done there and
-	// TLSClientConfig is ignored.
-	NetDialTLSContext func(ctx context.Context, network, addr string) (net.Conn, error)
-
-	// Proxy specifies a function to return a proxy for a given
-	// Request. If the function returns a non-nil error, the
-	// request is aborted with the provided error.
-	// If Proxy is nil or returns a nil *URL, no proxy is used.
-	Proxy func(*http.Request) (*url.URL, error)
-
-	// TLSClientConfig specifies the TLS configuration to use with tls.Client.
-	// If nil, the default configuration is used.
-	// If either NetDialTLS or NetDialTLSContext are set, Dial assumes the TLS handshake
-	// is done there and TLSClientConfig is ignored.
-	TLSClientConfig *tls.Config
-
-	// HandshakeTimeout specifies the duration for the handshake to complete.
-	HandshakeTimeout time.Duration
-
-	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer
-	// size is zero, then a useful default size is used. The I/O buffer sizes
-	// do not limit the size of the messages that can be sent or received.
-	ReadBufferSize, WriteBufferSize int
-
-	// WriteBufferPool is a pool of buffers for write operations. If the value
-	// is not set, then write buffers are allocated to the connection for the
-	// lifetime of the connection.
-	//
-	// A pool is most useful when the application has a modest volume of writes
-	// across a large number of connections.
-	//
-	// Applications should use a single pool for each unique value of
-	// WriteBufferSize.
-	WriteBufferPool BufferPool
-
-	// Subprotocols specifies the client's requested subprotocols.
-	Subprotocols []string
-
-	// EnableCompression specifies if the client should attempt to negotiate
-	// per message compression (RFC 7692). Setting this value to true does not
-	// guarantee that compression will be supported. Currently only "no context
-	// takeover" modes are supported.
-	EnableCompression bool
-
-	// Jar specifies the cookie jar.
-	// If Jar is nil, cookies are not sent in requests and ignored
-	// in responses.
-	Jar http.CookieJar
-}
-
-// Dial creates a new client connection by calling DialContext with a background context.
-func (d *Dialer) Dial(urlStr string, requestHeader http.Header) (*Conn, *http.Response, error) {
-	return d.DialContext(context.Background(), urlStr, requestHeader)
-}
-
-var errMalformedURL = errors.New("malformed ws or wss URL")
-
-func hostPortNoPort(u *url.URL) (hostPort, hostNoPort string) {
-	hostPort = u.Host
-	hostNoPort = u.Host
-	if i := strings.LastIndex(u.Host, ":"); i > strings.LastIndex(u.Host, "]") {
-		hostNoPort = hostNoPort[:i]
-	} else {
-		switch u.Scheme {
-		case "wss":
-			hostPort += ":443"
-		case "https":
-			hostPort += ":443"
-		default:
-			hostPort += ":80"
-		}
-	}
-	return hostPort, hostNoPort
-}
-
-// DefaultDialer is a dialer with all fields set to the default values.
-var DefaultDialer = &Dialer{
-	Proxy:            http.ProxyFromEnvironment,
-	HandshakeTimeout: 45 * time.Second,
-}
-
-// nilDialer is dialer to use when receiver is nil.
-var nilDialer = *DefaultDialer
-
-// DialContext creates a new client connection. Use requestHeader to specify the
-// origin (Origin), subprotocols (Sec-WebSocket-Protocol) and cookies (Cookie).
-// Use the response.Header to get the selected subprotocol
-// (Sec-WebSocket-Protocol) and cookies (Set-Cookie).
-//
-// The context will be used in the request and in the Dialer.
-//
-// If the WebSocket handshake fails, ErrBadHandshake is returned along with a
-// non-nil *http.Response so that callers can handle redirects, authentication,
-// etcetera. The response body may not contain the entire response and does not
-// need to be closed by the application.
-func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader http.Header) (*Conn, *http.Response, error) {
-	if d == nil {
-		d = &nilDialer
-	}
-
-	challengeKey, err := generateChallengeKey()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	u, err := url.Parse(urlStr)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	switch u.Scheme {
-	case "ws":
-		u.Scheme = "http"
-	case "wss":
-		u.Scheme = "https"
-	default:
-		return nil, nil, errMalformedURL
-	}
-
-	if u.User != nil {
-		// User name and password are not allowed in websocket URIs.
-		return nil, nil, errMalformedURL
-	}
-
-	req := &http.Request{
-		Method:     http.MethodGet,
-		URL:        u,
-		Proto:      "HTTP/1.1",
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header:     make(http.Header),
-		Host:       u.Host,
-	}
-	req = req.WithContext(ctx)
-
-	// Set the cookies present in the cookie jar of the dialer
-	if d.Jar != nil {
-		for _, cookie := range d.Jar.Cookies(u) {
-			req.AddCookie(cookie)
-		}
-	}
-
-	// Set the request headers using the capitalization for names and values in
-	// RFC examples. Although the capitalization shouldn't matter, there are
-	// servers that depend on it. The Header.Set method is not used because the
-	// method canonicalizes the header names.
-	req.Header["Upgrade"] = []string{"websocket"}
-	req.Header["Connection"] = []string{"Upgrade"}
-	req.Header["Sec-WebSocket-Key"] = []string{challengeKey}
-	req.Header["Sec-WebSocket-Version"] = []string{"13"}
-	if len(d.Subprotocols) > 0 {
-		req.Header["Sec-WebSocket-Protocol"] = []string{strings.Join(d.Subprotocols, ", ")}
-	}
-	for k, vs := range requestHeader {
-		switch {
-		case k == "Host":
-			if len(vs) > 0 {
-				req.Host = vs[0]
-			}
-		case k == "Upgrade" ||
-			k == "Connection" ||
-			k == "Sec-Websocket-Key" ||
-			k == "Sec-Websocket-Version" ||
-			//#nosec G101 (CWE-798): Potential HTTP request smuggling via parameter pollution
-			k == "Sec-Websocket-Extensions" ||
-			(k == "Sec-Websocket-Protocol" && len(d.Subprotocols) > 0):
-			return nil, nil, errors.New("websocket: duplicate header not allowed: " + k)
-		case k == "Sec-Websocket-Protocol":
-			req.Header["Sec-WebSocket-Protocol"] = vs
-		default:
-			req.Header[k] = vs
-		}
-	}
-
-	if d.EnableCompression {
-		req.Header["Sec-WebSocket-Extensions"] = []string{"permessage-deflate; server_no_context_takeover; client_no_context_takeover"}
-	}
-
-	if d.HandshakeTimeout != 0 {
-		var cancel func()
-		ctx, cancel = context.WithTimeout(ctx, d.HandshakeTimeout)
-		defer cancel()
-	}
-
-	// Get network dial function.
-	var netDial func(network, add string) (net.Conn, error)
-
-	switch u.Scheme {
-	case "http":
-		if d.NetDialContext != nil {
-			netDial = func(network, addr string) (net.Conn, error) {
-				return d.NetDialContext(ctx, network, addr)
-			}
-		} else if d.NetDial != nil {
-			netDial = d.NetDial
-		}
-	case "https":
-		if d.NetDialTLSContext != nil {
-			netDial = func(network, addr string) (net.Conn, error) {
-				return d.NetDialTLSContext(ctx, network, addr)
-			}
-		} else if d.NetDialContext != nil {
-			netDial = func(network, addr string) (net.Conn, error) {
-				return d.NetDialContext(ctx, network, addr)
-			}
-		} else if d.NetDial != nil {
-			netDial = d.NetDial
-		}
-	default:
-		return nil, nil, errMalformedURL
-	}
-
-	if netDial == nil {
-		netDialer := &net.Dialer{}
-		netDial = func(network, addr string) (net.Conn, error) {
-			return netDialer.DialContext(ctx, network, addr)
-		}
-	}
-
-	// If needed, wrap the dial function to set the connection deadline.
-	if deadline, ok := ctx.Deadline(); ok {
-		forwardDial := netDial
-		netDial = func(network, addr string) (net.Conn, error) {
-			c, err := forwardDial(network, addr)
-			if err != nil {
-				return nil, err
-			}
-			err = c.SetDeadline(deadline)
-			if err != nil {
-				if err := c.Close(); err != nil {
-					log.Printf("websocket: failed to close network connection: %v", err)
-				}
-				return nil, err
-			}
-			return c, nil
-		}
-	}
-
-	// If needed, wrap the dial function to connect through a proxy.
-	if d.Proxy != nil {
-		proxyURL, err := d.Proxy(req)
-		if err != nil {
-			return nil, nil, err
-		}
-		if proxyURL != nil {
-			dialer, err := proxy.FromURL(proxyURL, netDialerFunc(netDial))
-			if err != nil {
-				return nil, nil, err
-			}
-			netDial = dialer.Dial
-		}
-	}
-
-	hostPort, hostNoPort := hostPortNoPort(u)
-	trace := httptrace.ContextClientTrace(ctx)
-	if trace != nil && trace.GetConn != nil {
-		trace.GetConn(hostPort)
-	}
-
-	netConn, err := netDial("tcp", hostPort)
-	if err != nil {
-		return nil, nil, err
-	}
-	if trace != nil && trace.GotConn != nil {
-		trace.GotConn(httptrace.GotConnInfo{
-			Conn: netConn,
-		})
-	}
-
-	defer func() {
-		if netConn != nil {
-			if err := netConn.Close(); err != nil {
-				log.Printf("websocket: failed to close network connection: %v", err)
-			}
-		}
-	}()
-
-	if u.Scheme == "https" && d.NetDialTLSContext == nil {
-		// If NetDialTLSContext is set, assume that the TLS handshake has already been done
-
-		cfg := cloneTLSConfig(d.TLSClientConfig)
-		if cfg.ServerName == "" {
-			cfg.ServerName = hostNoPort
-		}
-		tlsConn := tls.Client(netConn, cfg)
-		netConn = tlsConn
-
-		if trace != nil && trace.TLSHandshakeStart != nil {
-			trace.TLSHandshakeStart()
-		}
-		err := doHandshake(ctx, tlsConn, cfg)
-		if trace != nil && trace.TLSHandshakeDone != nil {
-			trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
-		}
-
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-
-	conn := newConn(netConn, false, d.ReadBufferSize, d.WriteBufferSize, d.WriteBufferPool, nil, nil)
-
-	if err := req.Write(netConn); err != nil {
-		return nil, nil, err
-	}
-
-	if trace != nil && trace.GotFirstResponseByte != nil {
-		if peek, err := conn.br.Peek(1); err == nil && len(peek) == 1 {
-			trace.GotFirstResponseByte()
-		}
-	}
-
-	resp, err := http.ReadResponse(conn.br, req)
-	if err != nil {
-		if d.TLSClientConfig != nil {
-			for _, proto := range d.TLSClientConfig.NextProtos {
-				if proto != "http/1.1" {
-					return nil, nil, fmt.Errorf(
-						"websocket: protocol %q was given but is not supported;"+
-							"sharing tls.Config with net/http Transport can cause this error: %w",
-						proto, err,
-					)
-				}
-			}
-		}
-		return nil, nil, err
-	}
-
-	if d.Jar != nil {
-		if rc := resp.Cookies(); len(rc) > 0 {
-			d.Jar.SetCookies(u, rc)
-		}
-	}
-
-	if resp.StatusCode != 101 ||
-		!tokenListContainsValue(resp.Header, "Upgrade", "websocket") ||
-		!tokenListContainsValue(resp.Header, "Connection", "upgrade") ||
-		resp.Header.Get("Sec-Websocket-Accept") != computeAcceptKey(challengeKey) {
-		// Before closing the network connection on return from this
-		// function, slurp up some of the response to aid application
-		// debugging.
-		buf := make([]byte, 1024)
-		n, _ := io.ReadFull(resp.Body, buf)
-		resp.Body = io.NopCloser(bytes.NewReader(buf[:n]))
-		return nil, resp, ErrBadHandshake
-	}
-
-	for _, ext := range parseExtensions(resp.Header) {
-		if ext[""] != "permessage-deflate" {
-			continue
-		}
-		_, snct := ext["server_no_context_takeover"]
-		_, cnct := ext["client_no_context_takeover"]
-		if !snct || !cnct {
-			return nil, resp, errInvalidCompression
-		}
-		conn.newCompressionWriter = compressNoContextTakeover
-		conn.newDecompressionReader = decompressNoContextTakeover
-		break
-	}
-
-	resp.Body = io.NopCloser(bytes.NewReader([]byte{}))
-	conn.subprotocol = resp.Header.Get("Sec-Websocket-Protocol")
-
-	if err := netConn.SetDeadline(time.Time{}); err != nil {
-		return nil, nil, err
-	}
-	netConn = nil // to avoid close in defer.
-	return conn, resp, nil
-}
-
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {
-		return &tls.Config{MinVersion: tls.VersionTLS12}
-	}
-	return cfg.Clone()
-}

vendor/golang.org/x/net/internal/socks/client.go
@@ -1,168 +1,0 @@
-// Copyright 2018 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package socks
-
-import (
-	"context"
-	"errors"
-	"io"
-	"net"
-	"strconv"
-	"time"
-)
-
-var (
-	noDeadline   = time.Time{}
-	aLongTimeAgo = time.Unix(1, 0)
-)
-
-func (d *Dialer) connect(ctx context.Context, c net.Conn, address string) (_ net.Addr, ctxErr error) {
-	host, port, err := splitHostPort(address)
-	if err != nil {
-		return nil, err
-	}
-	if deadline, ok := ctx.Deadline(); ok && !deadline.IsZero() {
-		c.SetDeadline(deadline)
-		defer c.SetDeadline(noDeadline)
-	}
-	if ctx != context.Background() {
-		errCh := make(chan error, 1)
-		done := make(chan struct{})
-		defer func() {
-			close(done)
-			if ctxErr == nil {
-				ctxErr = <-errCh
-			}
-		}()
-		go func() {
-			select {
-			case <-ctx.Done():
-				c.SetDeadline(aLongTimeAgo)
-				errCh <- ctx.Err()
-			case <-done:
-				errCh <- nil
-			}
-		}()
-	}
-
-	b := make([]byte, 0, 6+len(host)) // the size here is just an estimate
-	b = append(b, Version5)
-	if len(d.AuthMethods) == 0 || d.Authenticate == nil {
-		b = append(b, 1, byte(AuthMethodNotRequired))
-	} else {
-		ams := d.AuthMethods
-		if len(ams) > 255 {
-			return nil, errors.New("too many authentication methods")
-		}
-		b = append(b, byte(len(ams)))
-		for _, am := range ams {
-			b = append(b, byte(am))
-		}
-	}
-	if _, ctxErr = c.Write(b); ctxErr != nil {
-		return
-	}
-
-	if _, ctxErr = io.ReadFull(c, b[:2]); ctxErr != nil {
-		return
-	}
-	if b[0] != Version5 {
-		return nil, errors.New("unexpected protocol version " + strconv.Itoa(int(b[0])))
-	}
-	am := AuthMethod(b[1])
-	if am == AuthMethodNoAcceptableMethods {
-		return nil, errors.New("no acceptable authentication methods")
-	}
-	if d.Authenticate != nil {
-		if ctxErr = d.Authenticate(ctx, c, am); ctxErr != nil {
-			return
-		}
-	}
-
-	b = b[:0]
-	b = append(b, Version5, byte(d.cmd), 0)
-	if ip := net.ParseIP(host); ip != nil {
-		if ip4 := ip.To4(); ip4 != nil {
-			b = append(b, AddrTypeIPv4)
-			b = append(b, ip4...)
-		} else if ip6 := ip.To16(); ip6 != nil {
-			b = append(b, AddrTypeIPv6)
-			b = append(b, ip6...)
-		} else {
-			return nil, errors.New("unknown address type")
-		}
-	} else {
-		if len(host) > 255 {
-			return nil, errors.New("FQDN too long")
-		}
-		b = append(b, AddrTypeFQDN)
-		b = append(b, byte(len(host)))
-		b = append(b, host...)
-	}
-	b = append(b, byte(port>>8), byte(port))
-	if _, ctxErr = c.Write(b); ctxErr != nil {
-		return
-	}
-
-	if _, ctxErr = io.ReadFull(c, b[:4]); ctxErr != nil {
-		return
-	}
-	if b[0] != Version5 {
-		return nil, errors.New("unexpected protocol version " + strconv.Itoa(int(b[0])))
-	}
-	if cmdErr := Reply(b[1]); cmdErr != StatusSucceeded {
-		return nil, errors.New("unknown error " + cmdErr.String())
-	}
-	if b[2] != 0 {
-		return nil, errors.New("non-zero reserved field")
-	}
-	l := 2
-	var a Addr
-	switch b[3] {
-	case AddrTypeIPv4:
-		l += net.IPv4len
-		a.IP = make(net.IP, net.IPv4len)
-	case AddrTypeIPv6:
-		l += net.IPv6len
-		a.IP = make(net.IP, net.IPv6len)
-	case AddrTypeFQDN:
-		if _, err := io.ReadFull(c, b[:1]); err != nil {
-			return nil, err
-		}
-		l += int(b[0])
-	default:
-		return nil, errors.New("unknown address type " + strconv.Itoa(int(b[3])))
-	}
-	if cap(b) < l {
-		b = make([]byte, l)
-	} else {
-		b = b[:l]
-	}
-	if _, ctxErr = io.ReadFull(c, b); ctxErr != nil {
-		return
-	}
-	if a.IP != nil {
-		copy(a.IP, b)
-	} else {
-		a.Name = string(b[:len(b)-2])
-	}
-	a.Port = int(b[len(b)-2])<<8 | int(b[len(b)-1])
-	return &a, nil
-}
-
-func splitHostPort(address string) (string, int, error) {
-	host, port, err := net.SplitHostPort(address)
-	if err != nil {
-		return "", 0, err
-	}
-	portnum, err := strconv.Atoi(port)
-	if err != nil {
-		return "", 0, err
-	}
-	if 1 > portnum || portnum > 0xffff {
-		return "", 0, errors.New("port number out of range " + port)
-	}
-	return host, portnum, nil
-}

commit da803e7bb20aa5c9a5a5adc0e0cc5052497bb71c

fix: Setting InsecureSkipVerify not handled for ws connections
Regression from fab8d68fcb38f74c06f859fcd81371f255bcbb12 which uses the
net/http.DefaultClient when connecting using websockets.
This fix is only useful for testing environments, this configuration
should never be used outside testing.

Ticket: ALV-306
Changelog: Commit
Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

api/http/client.go
@@ -73,15 +73,16 @@ func NewClient(
 	}
 	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.TLSClientConfig = tlsConfig.Clone()
+	client := &http.Client{
+		Transport: transport,
+	}
 
 	var localAuth = HTTPClient{
 		serverURL:  strings.TrimRight(cfg.ServerURL, "/"),
 		PrivateKey: cfg.GetPrivateKey(),
 		Identity:   cfg.GetIdentity(),
-		client: &http.Client{
-			Transport: transport,
-		},
-		wsClient: apiws.NewClient(tlsConfig),
+		client:     client,
+		wsClient:   apiws.NewClient(client),
 	}
 	return &localAuth, nil
 }

api/ws/websocket.go
@@ -16,7 +16,6 @@ package ws
 
 import (
 	"context"
-	"crypto/tls"
 	"errors"
 	"net/http"
 	"strings"
@@ -174,10 +173,14 @@ func (sock *socket) Value(key any) any {
 }
 
 // Client implements only parts of the api.Client interface
-type Client struct{}
+type Client struct {
+	httpClient *http.Client
+}
 
-func NewClient(tlsConfig *tls.Config) api.SocketClient {
-	return &Client{}
+func NewClient(httpClient *http.Client) api.SocketClient {
+	return &Client{
+		httpClient: httpClient,
+	}
 }
 
 func (c *Client) OpenSocket(ctx context.Context, authz *api.Authz) (api.Socket, error) {
@@ -189,10 +192,13 @@ func (c *Client) OpenSocket(ctx context.Context, authz *api.Authz) (api.Socket, 
 	//nolint: bodyclose
 	conn, rsp, err := websocket.Dial(ctx,
 		url,
-		&websocket.DialOptions{HTTPHeader: http.Header{
-			"Authorization": []string{"Bearer " + authz.Token},
-			"User-Agent":    []string{api.UserAgent()},
-		}},
+		&websocket.DialOptions{
+			HTTPHeader: http.Header{
+				"Authorization": []string{"Bearer " + authz.Token},
+				"User-Agent":    []string{api.UserAgent()},
+			},
+			HTTPClient: c.httpClient,
+		},
 	)
 	if err != nil {
 		return nil, err